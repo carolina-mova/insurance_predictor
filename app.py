e415359767dbbfe662363fc4572dfdfb89c49032
Carolina Mora Vázquez <carolinemova16@hotmail.com>
2026-04-04 19:17:07 -0600

    Última actualización

--- app.py
+++ app.py
@@ -59,6 +59,8 @@ def predict():
     
     return jsonify({"charges": round(prediction_original, 2)})
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     application.run(host='0.0.0.0', port=port)
